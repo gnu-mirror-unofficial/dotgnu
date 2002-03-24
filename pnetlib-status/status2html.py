99bc863764075f7298b0c0000670b274cc6729f4
t3rmin4t0r <t3rmin4t0r>
2002-03-24 17:41:09 +0000

    fixed color of images

--- pnetlib-status/status2html.py
+++ pnetlib-status/status2html.py
@@ -8,7 +8,7 @@ import xml.dom.minidom
 import string
 import sys
 from cvstree import *
-cvs_entries=cvsmoddate("/home/gopal/dotgnu/pnetlib/runtime")
+cvs_entries=cvsmoddate("/opt/cvs/dotgnu/pnetlib/runtime")
 #################TODO#####################
 #                                        #
 # * implement error messages             #
@@ -32,6 +32,18 @@ class classnode:
 		self.props=node.getElementsByTagName("property")
 		self.attrs=node.getElementsByTagName("attribute")
 		print "reading...."+self.fqname
+	def __repr__(self):
+		return "<class name=\""+self.fqname+"\">"
+
+# of internal call documentation
+class iclassnode:
+	def __init__(self,node):
+		self.node=node
+		self.name=node.getAttribute("name")
+		self.ns=node.getAttribute("namespace")
+		self.fqname=self.ns+"."+self.name
+		self.icalls=node.getElementsByTagName("internalcall")
+		print "reading...."+self.ns+self.name
 	def __repr__(self):
 		return "<class name=\""+self.fqname+"\">"
 #########################helper functions#########################
@@ -126,9 +138,16 @@ def write_pnetlib_footer(fp):
 	import time
 	lastd=time.gmtime(time.time());
 	print_curved_header(fp,"&nbsp;")
-	print_curved_title(fp,"""<p align="center"><font size="-3">
-							&copy; All copyrights to DotGNU.org<br>
-							Licensed under the GNU FDL.</font>
+	print_curved_title(fp,"""<p align="center">
+	This document is licensed under the GNU FDL.<br>
+	"""+"Last Updated on <i>"+`lastd[2]`+"-"+`lastd[1]`+"-"+`lastd[0]`+"</i>"
+	+"""
+							</p>
+							<p align="center">
+								Visit <a href="http://www.dotgnu.org">
+								http://www.dotgnu.org</a> for more details
+							</p>
+							<p align="center">
 							<table border="0" cellspacing="10" align="center">
 							<tr align="center">
 								<td><a href="http://www.fsf.org">
@@ -150,17 +169,14 @@ def write_pnetlib_footer(fp):
 							<td><font size="-5">Maintained by Gopal.V </font></td>
 							<td></td>
 							</tr>
-							</table>							
+							</table>
 							</p>
-							<p align="right">
-							<font size="-5">Last Updated on <i>"""+
-							`lastd[2]`+"-"+`lastd[1]`+"-"+`lastd[0]`+
-							"""</i></font></p>""")
+							""")
 	print_curved_footer(fp)
 #end format functions
 
 
-def print_namespace_list(nslist,ctor,method,field,prop,event,attr):
+def print_namespace_list(nslist,ctor,method,field,prop,event,attr,inslist,icall):
 	#nslist=nsdict.keys()
 	#nslist.sort()
 	fp=open("index.html","w")
@@ -180,6 +196,12 @@ def print_namespace_list(nslist,ctor,method,field,prop,event,attr):
 	fp.write("""	<tr><td>""")
 	for each in nslist:
 		fp.write("&nbsp&middot;&nbsp;<b><a href=\""+each+".html\">"+each+"</a></b><br>")
+	fp.write("""	</td></tr>""")
+	fp.write("""	Runtime Methods<br>""")
+	for each in inslist:
+		fp.write("&nbsp&middot;&nbsp;<b><a href=\"icall."+each+".html\">"+each+"</a></b><br>")
+	fp.write("""	<tr><td>""")
+	fp.write("""	</td></tr>""")
 	fp.write("""	</table>""")
 	fp.write("<!--CENTER BLOCKS-->")
 	fp.write("""</td><td valign="top" border="2">""")
@@ -192,7 +214,8 @@ def print_namespace_list(nslist,ctor,method,field,prop,event,attr):
 	fp.write("<b>"+`field`+"</b> fields, ")
 	fp.write("<b>"+`prop`+"</b> properties, ")
 	fp.write("<b>"+`event`+"</b> events and ")
-	fp.write("<b>"+`attr`+"</b> attributes.")
+	fp.write("<b>"+`attr`+"</b> attributes and")
+	fp.write("<b>"+`icall`+"</b> internal calls")
 	fp.write("""	</td></tr>""")
 	fp.write("""	</table>""")
 #	fp.write("""</td><td align="right" valign="top" border="2">""")
@@ -203,7 +226,7 @@ def print_namespace_list(nslist,ctor,method,field,prop,event,attr):
 	fp.write("</td></tr>")
 	fp.write("</table>")
 	fp.write("</td><td>&nbsp;</td></tr>")
-	fp.write("""<tr><td></td><td align="right"><font size="-3">[<a href="status.xml">XML</a> | <a href="status2html.py">PYTHON</a>]</font></td><td></td></tr>""");
+	fp.write("""<tr><td></td><td align="right"><font size="-3">[<a href="status.xml.gz">XML</a> | <a href="status2html.py">PYTHON</a>]</font></td><td></td></tr>""");
 	print_curved_footer(fp)
 	write_pnetlib_footer(fp)
 	fp.write("</body></html>")
@@ -394,9 +417,106 @@ def print_namespace_inner(fname,classlist,prev,next):
 	fp.write("</body></html>")
 	fp.close()
 	return (ctorcount,methodcount,fieldcount,propcount,eventcount,attrcount)
+#########################################################################################################
+def print_icall_method_summary(fp,methods):
+	fp.write("<tr><td>")
+	fp.write("""<table cellspacing="0" width="100%" bgcolor="#b2b297" border="1">""")
+	fp.write("""<tr><td><font size="+2">InternalCall Summary</h3></td></tr>""")
+	fp.write("</table>")
+	fp.write("""<table cellpadding="4" cellspacing="0" width="100%" bordercolor="#99997e" border="1">""")
+	for each in methods:
+		msg="<font color=\"#ff1212\">"+each.getAttribute("status")+"</font>"
+		sig=each.getAttribute("signature")
+		(prefix,name,params)=split_signature(sig)
+		fp.write("<tr border=\"1\"><td><tt>"+prefix+"</tt><b> "+name+"</b><i>("+params+"</i></td><td align=\"right\">"+msg+"</td></tr>")
+	fp.write("</table>")
+	fp.write("</td></tr>")
 	
+def print_icall_each_class(fp,cnode):
+	fp.write("<a name=\""+cnode.name+"\">")
+	fp.write("<table bordercolor=\"#dedebb\" border=\"1\" width=\"90%\">")
+	fp.write("<tr><td><h2>"+cnode.fqname+"</h2></td></tr>")
+	fp.write("</td></tr>")
+	#summary
+	print_icall_method_summary(fp,cnode.icalls)
+	fp.write("</table>")
+	fp.write("<br>")
+	fp.write("</a>")
+	
+def print_icall_ns_inner(fname,classlist,prev,next):
+	missing=0
+	fp=open("icall."+fname+".html","w")
+	print "writing.....icall."+fname+".html"
+	fp.write("""<html><body bgcolor="#505050" link="#0A0A0A" vlink="#303030">""")
+	write_pnetlib_header(fp)
+	print_curved_title(fp,"<h2 align=\"left\">"+fname+" Namespace</h2>")
+	links="<p align=\"right\">"
+	if(prev!="index.html"):
+		links=links+"<a href=\"icall."+prev+"\"><img src=\"larrow.jpg\" border=\"0\"></a>"
+	links=links+"<a href=\"index.html\"><img src=\"uarrow.jpg\" border=\"0\"></a>"
+	if(next!="index.html"):
+		links=links+"<a href=\"icall."+next+"\"><img src=\"rarrow.jpg\" border=\"0\"></a>"
+	links=links+"</p>"	
+	print_curved_title(fp,links)
+	fp.write("<tr><td>&nbsp;</td><td>")
+	fp.write("<!--DATA REGION-->")
+	fp.write("""<table bordercolor="#dedebb">""")
+	fp.write("""<tr><td border="1">""")
+	fp.write("<!--LEFT BLOCK-->")
+	fp.write("""	<table align="center" bgcolor="#cfcfbb">""")
+	fp.write("""	<tr><td>Classes </td></tr>""")
+	fp.write("""	<tr><td>""")
+	for each in classlist:
+		fp.write("&nbsp;&middot;&nbsp<b><a href=\"#"+each.name+"\">"+each.name+"</a></b><br>")
+		missing=missing+len(each.icalls)
+	fp.write("""	</td></tr>""")
+	fp.write("""	</table>""")
+	fp.write("""</td><td valign="top">""")
+	fp.write("<!--CENTER BLOCK-->")
+	fp.write("	<table><tr><td>")
+	fp.write("<p>There are <b>"+`len(classlist)`+"</b> classes with missing InternalCalls in this namespace</p>")
+	fp.write("<b>"+`missing`+"</b> internal calls missing")
+	fp.write("</td></tr></table>")
+	fp.write("""	</td></tr>""")
+	fp.write("""	</table>""")
+	fp.write("</td><td>&nbsp;</td></tr>")
+#classwise printing starts
+	for each in classlist:
+		fp.write("""<tr><td>&nbsp;</td><td>&nbsp;<hr color="#dedebb">&nbsp;</td><td>&nbsp;</td></tr>""")
+		fp.write("""<tr align="center"><td>&nbsp;</td><td>""")
+		print_icall_each_class(fp,each)
+		fp.write("</td><td>&nbsp;</td></tr>")
+	print_curved_footer(fp)
+	write_pnetlib_footer(fp)
+	fp.write("</body></html>")
+	fp.close()
+	return missing
+
+def process_icall():
+	missing=0
+	inodes=[]
+	insdict={}
+	iroot=xml.dom.minidom.parse('icall.xml')
+	icalls=iroot.getElementsByTagName('class');
+	for each in icalls:
+		c=iclassnode(each);
+		inodes.append(c);
+		if(insdict.has_key(c.ns)):
+			a=insdict.get(c.ns)
+		else:
+			a=[]
+		a.append(c)
+		insdict.update({c.ns:a})
+	inslist=insdict.keys()
+	inslist.sort()
+	for each in inslist:
+		(prev,next)=get_prev_next(inslist,each)
+		missing=missing+print_icall_ns_inner(each,insdict.get(each),prev,next)
+	return (inslist,missing)
+
 if __name__=="__main__":
 	root=xml.dom.minidom.parse('status.xml')
+	(inslist,icall)=process_icall()
 	classes=root.getElementsByTagName("class")
 	classnodes=[]
 	nsdict={}
@@ -427,5 +547,5 @@ if __name__=="__main__":
 		eventcount=eventcount+event
 		methodcount=methodcount+method
 		attrcount=attrcount+attr
-	print_namespace_list(nslist,ctorcount,methodcount,fieldcount,propcount,eventcount,attrcount)
+	print_namespace_list(nslist,ctorcount,methodcount,fieldcount,propcount,eventcount,attrcount,inslist,icall)
 	
